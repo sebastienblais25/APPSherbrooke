c9e32419e71161f2300d5c00389eee315a1c3d9a
sebastien.blais25 <jean-sebastien.bruneau-blais@usherbrooke.ca>
2021-02-18 14:46:50 -0500

    ProjectBeginning

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import numpy
 import os
 from osgeo import ogr
-import gdal
+from osgeo import gdal
+from osgeo import gdalconst
 ## sherbrooke 
 
 driver = ogr.GetDriverByName("OpenFileGDB")
+
 print(driver)
-ds = driver.Open(r"D:\APP_data\APP_PI.gdb", 0)
+sr = driver.Open(r"D:\APP_data\APP_PI.gdb", 0)
+raster = gdal.Open(r'D:\dumping_codes\Tiff\test.tiff')
+print(sr)
+lyr = sr.GetLayer("UE")
+# geot = raster.GetGeoTranform()
 
-ds.GetLayer("UE")
-#<osgeo.ogr.Layer; proxy of <Swig Object of type 'OGRLayerShadow *' at 0x02BB7050> >
-print(ds)
-sr = ds.GetLayer("UE")
-#<osgeo.osr.SpatialReference; proxy of <Swig Object of type 'OSRSpatialReferenceShadow *' at 0x02BB7080> >
-sr.ExportToProj4()
+# drv_tiff = gdal.GetDriverByName("GTiff") 
+# chn_ras_ds = drv_tiff.Create(out_net, raster.RasterXSize, raster.RasterYSize, 1, gdal.GDT_Float32)
+# chn_ras_ds.SetGeoTransform(geot)
 
+gdal.Rasterize(raster, lyr)
 
-
-#Layer Criteria
-
-# Layer factor
-
-
+# if ds.RasterCount > 0 and ds.GetLayerCount() > 0:
+#     print('Raster and vector')
+# elif ds.RasterCount > 0:
+#     print('Raster')
+# elif ds.GetLayerCount() > 0:
+#     print(' vector')
+    
